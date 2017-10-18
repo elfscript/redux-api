e88e2a8c233568cb5476dc3db6f4645d1dd7ab96
3hdeng <3hdeng@gmail.com>
2017-10-18 22:11:19 +0800

    try react-contextmenu

--- examples/todos/src/components/App.js
+++ examples/todos/src/components/App.js
@@ -5,6 +5,7 @@ import AddTodo from '../containers/AddTodo';
 import UpdateDB from '../containers/UpdateDB';
 import VisibleTodoList from '../containers/VisibleTodoList'
 import rest from "../actions/rest"
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 /*const App = () => (
 	<div>
@@ -16,6 +17,9 @@ import rest from "../actions/rest"
 
 export default App
 */
+// contextmenu event not work ?
+// try react-contextmenu
+
 //===
 class App extends React.Component {
 	constructor(props) {
@@ -24,6 +28,12 @@ class App extends React.Component {
 		this.state = {
 			isReady: false
 		};
+		this.handleClick=(e, data)=> {
+			console.log(data);
+		}
+                this.handleClick2=(s) => { console.log(s);}
+
+
 	}
 
 
@@ -31,11 +41,31 @@ class App extends React.Component {
 	componentDidMount() {
 		this.props.getTodosFromServer();
 	}
-
 	render() {
 		return this.props.isReady
 			?  (
 				<div>
+					<menu type="context" id="mycontextMenu">
+						<menuitem label="edit" onClick={this.handleClick2('edit clicked')}></menuitem>
+						<menuitem label="delete" onClick={this.handleClick2('delete clicked')}></menuitem>
+					</menu>
+					<div contextMenu="mycontextMenu" >right-click test</div>
+					<ContextMenuTrigger id="some_unique_identifier">
+						<div className="well">Right click to see the menu</div>
+					</ContextMenuTrigger>
+
+					<ContextMenu id="some_unique_identifier">
+						<MenuItem data={{text:"some_data1"}} onClick={this.handleClick}>
+							ContextMenu Item 1
+						</MenuItem>
+						<MenuItem data={{text:"some_data2"}} onClick={this.handleClick}>
+							ContextMenu Item 2
+						</MenuItem>
+						<MenuItem divider />
+						<MenuItem data={{text:"some_data3"}} onClick={this.handleClick}>
+							ContextMenu Item 3
+						</MenuItem>
+					</ContextMenu>
 					<span><AddTodo /><UpdateDB /></span>
 					<VisibleTodoList />
 					<Footer />
